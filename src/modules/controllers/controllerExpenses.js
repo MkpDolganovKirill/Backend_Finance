2d77bccf15396d426617cc3172fbc0929a149220
Dolganov Kirill <dolganovk.exceedteam@gmail.com>
2022-03-31 11:16:38 +0300

    Added router for get all expenses

---
+++ src/modules/controllers/controllerExpenses.js
@@ -0,0 +1,10 @@
+const ExpensesSchema = require('../../db/models/expenses/index');
+
+module.exports.getAllExpenses = async (req, res) => {
+  try {
+    const result = await ExpensesSchema.find();
+    res.send({data: result});
+  } catch (error) {
+    return res.status(422).send({error: error, messaage: 'Error!'});
+  };
+};
