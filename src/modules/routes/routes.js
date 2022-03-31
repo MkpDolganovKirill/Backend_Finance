dbab583823e1ec77d3f77356b819b041bd577490
Dolganov Kirill <dolganovk.exceedteam@gmail.com>
2022-03-31 12:22:30 +0300

    Added create expense router.

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getAllExpenses
+  getAllExpenses,
+  createNewExpense
 } = require('../controllers/controllerExpenses');
 
 router.get('/allExpenses', getAllExpenses);
+router.post('/createNewExpense', createNewExpense);
 
 module.exports = router;
